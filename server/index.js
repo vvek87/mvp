753f6f649131d9384988f25e713498f98580ffba
vvek87 <vvek87@gmal.com>
2019-07-29 17:29:12 -0500

    remove extra space and add images folder

--- server/index.js
+++ server/index.js
@@ -6,22 +6,16 @@ const port = 3002;
 const webshot = require('webshot');
 const fs = require('fs');
 const path = require('path');
-
-
 const hash = require('string-hash');
-
 
 app.use(express.static(__dirname + '/../public/'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
 app.get('/', (req, res) => {
   res.send(200);
 });
-
 
 app.post('/submit', (req, res) => {
   let checkedLink = req.body.newLink;
@@ -30,7 +24,7 @@ app.post('/submit', (req, res) => {
       checkedLink = 'https://'.concat(checkedLink);
     }
 
-  let renderStream = webshot(req.body.newLink)
+  let renderStream = webshot(req.body.newLink);
   let writeSite = fs.createWriteStream(path.join(__dirname, '../public/images/' + hash(req.body.newLink) + '.png'));
 
   renderStream.on('data', (chunk) => {
@@ -41,7 +35,6 @@ app.post('/submit', (req, res) => {
     res.send(req.body.newLink);
   })
 });
-
 
 app.listen(port, () => {
   console.log('Listening on port: ', port);
